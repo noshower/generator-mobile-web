47fda830ab79d7727bd4b7924ecb141e29b1d613
吴震宇 <wu.zhenyu@darcytech.com>
2020-11-05 17:37:37 +0800

    build: 修改commit 配置

--- generators/templates/.commitlintrc.js
+++ generators/templates/.commitlintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'chore', 'publish']],
-    'subject-case': [0, 'never'],
-  },
 };
